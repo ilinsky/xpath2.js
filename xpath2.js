adcd70a8bf3aa2b7b76479e749509949ac8647cd
ilinsky <sergey@ilinsky.com>
2012-10-18 10:10:15 +0200

    Using .files in source runner

--- xpath2.js
+++ xpath2.js
@@ -8,91 +8,31 @@
  */
 
 (function() {
-	var files	= [];
-	// Import objects from environment
-	files.push("src/import.js");
-	// Helper classes
-	files.push("src/helpers/DOMAdapter.js");
-	files.push("src/helpers/XPath2Error.js");
-	files.push("src/helpers/XPath2Lexer.js");
-	files.push("src/helpers/XPath2Parser.js");
-	files.push("src/helpers/XPath2Context.js");
-	files.push("src/helpers/XPath2Sequence.js");
-	//
-	files.push("src/helpers/XPath2.js");
-	//
-	files.push("src/expressions/Expr.js");
-	files.push("src/expressions/ExprSingle.js");
-	//
-	files.push("src/expressions/for/ForExpr.js");
-	//
-	files.push("src/expressions/if/IfExpr.js");
-	//
-	files.push("src/expressions/quantified/QuantifiedExpr.js");
-	//
-	files.push("src/expressions/comparison/ComparisonExpr.js");
-	//
-	files.push("src/expressions/arithmetic/AdditiveExpr.js");
-	files.push("src/expressions/arithmetic/MultiplicativeExpr.js");
-	files.push("src/expressions/arithmetic/UnaryExpr.js");
-	files.push("src/expressions/arithmetic/ValueExpr.js");
-	//
-	files.push("src/expressions/logical/OrExpr.js");
-	files.push("src/expressions/logical/AndExpr.js");
-	//
-	files.push("src/expressions/path/AxisStep.js");
-	files.push("src/expressions/path/PathExpr.js");
-	files.push("src/expressions/path/StepExpr.js");
-	// Tests
-	files.push("src/expressions/path/tests/NodeTest.js");
-	files.push("src/expressions/path/tests/KindTest.js");
-	files.push("src/expressions/path/tests/NameTest.js");
-	//
-	files.push("src/expressions/primary/PrimaryExpr.js");
-	files.push("src/expressions/primary/ParenthesizedExpr.js");
-	files.push("src/expressions/primary/ContextItemExpr.js");
-	files.push("src/expressions/primary/Literal.js");
-	files.push("src/expressions/primary/NumericLiteral.js");
-	files.push("src/expressions/primary/StringLiteral.js");
-	files.push("src/expressions/primary/FilterExpr.js");
-	files.push("src/expressions/primary/VarRef.js");
-	files.push("src/expressions/primary/FunctionCall.js");
-	//
-	files.push("src/expressions/sequence/IntersectExceptExpr.js");
-	files.push("src/expressions/sequence/RangeExpr.js");
-	files.push("src/expressions/sequence/UnionExpr.js");
-	//
-//	files.push("src/expressions/InstanceofExpr.js");
-//	files.push("src/expressions/TreatExpr.js");
-//	files.push("src/expressions/CastableExpr.js");
-//	files.push("src/expressions/CastExpr.js");
-	// Functions
-	files.push("src/functions/accessor.js");
-	files.push("src/functions/anyuri.js");
-	files.push("src/functions/binary.js");
-	files.push("src/functions/boolean.js");
-	files.push("src/functions/context.js");
-	files.push("src/functions/date.js");
-	files.push("src/functions/node.js");
-	files.push("src/functions/notation.js");
-	files.push("src/functions/numeric.js");
-	files.push("src/functions/qname.js");
-	files.push("src/functions/sequence.js");
-	files.push("src/functions/string.js");
-	files.push("src/functions/trace.js");
 
-	// Export objects to environment
-	files.push("export.js");
-
-	// load files
 	var source	= [],
 		scripts	= document.getElementsByTagName("script"),
-		base	= scripts[scripts.length-1].src.replace(/\/?[^\/]+$/, '');
-	for (var n = 0; n < files.length; n++) {
-		var oRequest	= new (window.XMLHttpRequest ? XMLHttpRequest : ActiveXObject("Microsoft.XMLHTTP"));
-		oRequest.open("GET", base + '/' + files[n], false);
-		oRequest.send(null);
-		source[source.length]	= oRequest.responseText;
+		path	= scripts[scripts.length-1].src.replace(/\/?[^\/]+$/, '/'),
+		cXMLHttpRequest	= window.XMLHttpRequest || function() {
+			return new ActiveXObject("Microsoft.XMLHTTP");
+		};
+
+	// get files list
+	var oRequest	= new cXMLHttpRequest;
+	oRequest.open("GET", path + "src/" + ".files", false);
+	oRequest.send(null);
+	var files	= oRequest.responseText.split(/\n/g);
+
+	// Add export
+	files.push("../export.js");
+
+	// load files
+	for (var n = 0, file; n < files.length; n++) {
+		if ((file = files[n].replace(/^\s+/, "").replace(/\s+$/, "")) != '' && file.substr(0, 1) != "#") {
+			var oRequest	= new cXMLHttpRequest;
+			oRequest.open("GET", path + "src/" + file, false);
+			oRequest.send(null);
+			source[source.length]	= oRequest.responseText;
+		}
 	}
 	var oScript	= document.getElementsByTagName("head")[0].appendChild(document.createElement("script"));
 	oScript.type	= "text/javascript";
