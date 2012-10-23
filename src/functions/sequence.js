98c3552890f52daf9dfb871d0c183c3696da2384
ilinsky <sergey@ilinsky.com>
2012-10-23 16:01:33 +0200

    Added function placehodlers

--- src/functions/sequence.js
+++ src/functions/sequence.js
@@ -50,16 +50,151 @@
 
 */
 
+// 15.1 General Functions and Operators on Sequences
 // fn:boolean($arg as item()*) as xs:boolean
-cFunctionCall.functions["boolean"]	= function(oSequence) {
+cFunctionCall.functions["boolean"]	= function(oSequence1) {
 	if (!arguments.length)
 		throw new cXPath2Error("XPST0017");
-	return new cXPath2Sequence(oSequence.toBoolean());
+	return new cXPath2Sequence(oSequence1.toBoolean());
 };
 
+// fn:index-of($seqParam as xs:anyAtomicType*, $srchParam as xs:anyAtomicType) as xs:integer*
+// fn:index-of($seqParam as xs:anyAtomicType*, $srchParam as xs:anyAtomicType, $collation as xs:string) as xs:integer*
+cFunctionCall.functions["index-of"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "index-of" + "' not implemented";
+};
+
+// fn:empty($arg as item()*) as xs:boolean
+cFunctionCall.functions["empty"]	= function(oSequence1) {
+	throw "Function '" + "empty" + "' not implemented";
+};
+
+// fn:exists($arg as item()*) as xs:boolean
+cFunctionCall.functions["exists"]	= function(oSequence1) {
+	throw "Function '" + "exists" + "' not implemented";
+};
+
+// fn:distinct-values($arg as xs:anyAtomicType*) as xs:anyAtomicType*
+// fn:distinct-values($arg as xs:anyAtomicType*, $collation as xs:string) as xs:anyAtomicType*
+cFunctionCall.functions["distinct-values"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "distinct-values" + "' not implemented";
+};
+
+// fn:insert-before($target as item()*, $position as xs:integer, $inserts as item()*) as item()*
+cFunctionCall.functions["insert-before"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "insert-before" + "' not implemented";
+};
+
+// fn:remove($target as item()*, $position as xs:integer) as item()*
+cFunctionCall.functions["remove"]	= function(oSequence1, oSequence1) {
+	throw "Function '" + "remove" + "' not implemented";
+};
+
+// fn:reverse($arg as item()*) as item()*
+cFunctionCall.functions["reverse"]	= function(oSequence1) {
+	throw "Function '" + "reverse" + "' not implemented";
+};
+
+// fn:subsequence($sourceSeq as item()*, $startingLoc as xs:double) as item()*
+// fn:subsequence($sourceSeq as item()*, $startingLoc as xs:double, $length as xs:double) as item()*
+cFunctionCall.functions["subsequence"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "subsequence" + "' not implemented";
+};
+
+// fn:unordered($sourceSeq as item()*) as item()*
+cFunctionCall.functions["unordered"]	= function(oSequence1) {
+	throw "Function '" + "unordered" + "' not implemented";
+};
+
+
+// 15.2 Functions That Test the Cardinality of Sequences
+// fn:zero-or-one($arg as item()*) as item()?
+cFunctionCall.functions["zero-or-one"]	= function(oSequence1) {
+	throw "Function '" + "zero-or-one" + "' not implemented";
+};
+
+// fn:one-or-more($arg as item()*) as item()+
+cFunctionCall.functions["one-or-more"]	= function(oSequence1) {
+	throw "Function '" + "one-or-more" + "' not implemented";
+};
+
+// fn:exactly-one($arg as item()*) as item()
+cFunctionCall.functions["exactly-one"]	= function(oSequence1) {
+	throw "Function '" + "exactly-one" + "' not implemented";
+};
+
+
+// 15.3 Equals, Union, Intersection and Except
+// fn:deep-equal($parameter1 as item()*, $parameter2 as item()*) as xs:boolean
+// fn:deep-equal($parameter1 as item()*, $parameter2 as item()*, $collation as string) as xs:boolean
+cFunctionCall.functions["deep-equal"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "deep-equal" + "' not implemented";
+};
+
+// 15.4 Aggregate Functions
 // fn:count($arg as item()*) as xs:integer
-cFunctionCall.functions["count"]	= function(oSequence) {
+cFunctionCall.functions["count"]	= function(oSequence1) {
 	if (!arguments.length)
 		throw new cXPath2Error("XPST0017");
-	return new cXPath2Sequence(oSequence.items.length);
+	return new cXPath2Sequence(oSequence1.items.length);
 };
+
+// fn:avg($arg as xs:anyAtomicType*) as xs:anyAtomicType?
+cFunctionCall.functions["avg"]	= function(oSequence1) {
+	throw "Function '" + "avg" + "' not implemented";
+};
+
+// fn:max($arg as xs:anyAtomicType*) as xs:anyAtomicType?
+// fn:max($arg as xs:anyAtomicType*, $collation as string) as xs:anyAtomicType?
+cFunctionCall.functions["max"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "max" + "' not implemented";
+};
+
+// fn:min($arg as xs:anyAtomicType*) as xs:anyAtomicType?
+// fn:min($arg as xs:anyAtomicType*, $collation as string) as xs:anyAtomicType?
+cFunctionCall.functions["min"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "min" + "' not implemented";
+};
+
+// fn:sum($arg as xs:anyAtomicType*) as xs:anyAtomicType
+// fn:sum($arg as xs:anyAtomicType*, $zero as xs:anyAtomicType?) as xs:anyAtomicType?
+cFunctionCall.functions["sum"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "sum" + "' not implemented";
+};
+
+
+// 15.5 Functions and Operators that Generate Sequences
+// fn:id($arg as xs:string*) as element()*
+// fn:id($arg as xs:string*, $node as node()) as element()*
+cFunctionCall.functions["id"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "id" + "' not implemented";
+};
+
+// fn:idref($arg as xs:string*) as node()*
+// fn:idref($arg as xs:string*, $node as node()) as node()*
+cFunctionCall.functions["idref"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "idref" + "' not implemented";
+};
+
+// fn:doc($uri as xs:string?) as document-node()?
+cFunctionCall.functions["doc"]	= function(oSequence1) {
+	throw "Function '" + "doc" + "' not implemented";
+};
+
+// fn:doc-available($uri as xs:string?) as xs:boolean
+cFunctionCall.functions["doc-available"]	= function(oSequence1) {
+	throw "Function '" + "doc-available" + "' not implemented";
+};
+
+// fn:collection() as node()*
+// fn:collection($arg as xs:string?) as node()*
+cFunctionCall.functions["collection"]	= function(oSequence1) {
+	throw "Function '" + "collection" + "' not implemented";
+};
+
+// fn:element-with-id($arg as xs:string*) as element()*
+// fn:element-with-id($arg as xs:string*, $node as node()) as element()*
+cFunctionCall.functions["element-with-id"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "element-with-id" + "' not implemented";
+};
+
--- src/functions/string.js
+++ src/functions/string.js
@@ -41,6 +41,147 @@
 		matches
 		replace
 		tokenize
-
-
 */
+
+// 7.2 Functions to Assemble and Disassemble Strings
+// fn:codepoints-to-string($arg as xs:integer*) as xs:string
+cFunctionCall.functions["codepoints-to-string"]	= function(oSequence1) {
+	throw "Function '" + "codepoints-to-string" + "' not implemented";
+};
+
+// fn:string-to-codepoints($arg as xs:string?) as xs:integer*
+cFunctionCall.functions["string-to-codepoints"]	= function(oSequence1) {
+	throw "Function '" + "string-to-codepoints" + "' not implemented";
+};
+
+// 7.3 Equality and Comparison of Strings
+// fn:compare($comparand1 as xs:string?, $comparand2 as xs:string?) as xs:integer?
+// fn:compare($comparand1 as xs:string?, $comparand2 as xs:string?, $collation as xs:string) as xs:integer?
+cFunctionCall.functions["compare"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "compare" + "' not implemented";
+};
+
+// fn:codepoint-equal($comparand1 as xs:string?, $comparand2  as xs:string?) as xs:boolean?
+cFunctionCall.functions["codepoint-equal"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "codepoint-equal" + "' not implemented";
+};
+
+
+// 7.4 Functions on String Values
+// fn:concat($arg1 as xs:anyAtomicType?, $arg2 as xs:anyAtomicType?, ...) as xs:string
+cFunctionCall.functions["concat"]	= function(oSequence1, oSequence2) {
+	if (arguments.length < 2)
+		throw new cXPath2Error("XPST0017");
+	throw "Function '" + "concat" + "' not implemented";
+};
+
+// fn:string-join($arg1 as xs:string*, $arg2 as xs:string) as xs:string
+cFunctionCall.functions["string-join"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "string-join" + "' not implemented";
+};
+
+// fn:substring($sourceString as xs:string?, $startingLoc as xs:double) as xs:string
+// fn:substring($sourceString as xs:string?, $startingLoc as xs:double, $length as xs:double) as xs:string
+cFunctionCall.functions["substring"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "substring" + "' not implemented";
+};
+
+// fn:string-length() as xs:integer
+// fn:string-length($arg as xs:string?) as xs:integer
+cFunctionCall.functions["string-length"]	= function(oSequence1) {
+	throw "Function '" + "string-length" + "' not implemented";
+};
+
+// fn:normalize-space() as xs:string
+// fn:normalize-space($arg as xs:string?) as xs:string
+cFunctionCall.functions["normalize-space"]	= function(oSequence1) {
+	throw "Function '" + "normalize-space" + "' not implemented";
+};
+
+// fn:normalize-unicode($arg as xs:string?) as xs:string
+// fn:normalize-unicode($arg as xs:string?, $normalizationForm as xs:string) as xs:string
+cFunctionCall.functions["normalize-unicode"]	= function(oSequence1, oSequence2) {
+	throw "Function '" + "normalize-unicode" + "' not implemented";
+};
+
+// fn:upper-case($arg as xs:string?) as xs:string
+cFunctionCall.functions["upper-case"]	= function(oSequence1) {
+	throw "Function '" + "upper-case" + "' not implemented";
+};
+
+// fn:lower-case($arg as xs:string?) as xs:string
+cFunctionCall.functions["lower-case"]	= function(oSequence1) {
+	throw "Function '" + "lower-case" + "' not implemented";
+};
+
+// fn:translate($arg as xs:string?, $mapString as xs:string, $transString as xs:string) as xs:string
+cFunctionCall.functions["translate"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "translate" + "' not implemented";
+};
+
+// fn:encode-for-uri($uri-part as xs:string?) as xs:string
+cFunctionCall.functions["encode-for-uri"]	= function(oSequence1) {
+	throw "Function '" + "encode-for-uri" + "' not implemented";
+};
+
+// fn:iri-to-uri($iri as xs:string?) as xs:string
+cFunctionCall.functions["iri-to-uri"]	= function(oSequence1) {
+	throw "Function '" + "iri-to-uri" + "' not implemented";
+};
+
+// fn:escape-html-uri($uri as xs:string?) as xs:string
+cFunctionCall.functions["escape-html-uri"]	= function(oSequence1) {
+	throw "Function '" + "escape-html-uri" + "' not implemented";
+};
+
+
+// 7.5 Functions Based on Substring Matching
+// fn:contains($arg1 as xs:string?, $arg2 as xs:string?) as xs:boolean
+// fn:contains($arg1 as xs:string?, $arg2 as xs:string?, $collation as xs:string) as xs:boolean
+cFunctionCall.functions["contains"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "contains" + "' not implemented";
+};
+
+// fn:starts-with($arg1 as xs:string?, $arg2 as xs:string?) as xs:boolean
+// fn:starts-with($arg1 as xs:string?, $arg2 as xs:string?, $collation as xs:string) as xs:boolean
+cFunctionCall.functions["starts-with"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "starts-with" + "' not implemented";
+};
+
+// fn:ends-with($arg1 as xs:string?, $arg2 as xs:string?) as xs:boolean
+// fn:ends-with($arg1 as xs:string?, $arg2 as xs:string?, $collation as xs:string) as xs:boolean
+cFunctionCall.functions["ends-with"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "ends-with" + "' not implemented";
+};
+
+// fn:substring-before($arg1 as xs:string?, $arg2 as xs:string?) as xs:string
+// fn:substring-before($arg1 as xs:string?, $arg2 as xs:string?, $collation as xs:string) as xs:string
+cFunctionCall.functions["substring-before"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "substring-before" + "' not implemented";
+};
+
+// fn:substring-after($arg1 as xs:string?, $arg2 as xs:string?) as xs:string
+// fn:substring-after($arg1 as xs:string?, $arg2 as xs:string?, $collation as xs:string) as xs:string
+cFunctionCall.functions["substring-after"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "substring-after" + "' not implemented";
+};
+
+
+// 7.6 String Functions that Use Pattern Matching
+// fn:matches($input as xs:string?, $pattern as xs:string) as xs:boolean
+// fn:matches($input as xs:string?, $pattern as xs:string, $flags as xs:string) as xs:boolean
+cFunctionCall.functions["matches"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "matches" + "' not implemented";
+};
+
+// fn:replace($input as xs:string?, $pattern as xs:string, $replacement as xs:string) as xs:string
+// fn:replace($input as xs:string?, $pattern as xs:string, $replacement as xs:string, $flags as xs:string) as xs:string
+cFunctionCall.functions["replace"]	= function(oSequence1, oSequence2, oSequence3, oSequence4) {
+	throw "Function '" + "replace" + "' not implemented";
+};
+
+// fn:tokenize($input as xs:string?, $pattern as xs:string) as xs:string*
+// fn:tokenize($input as xs:string?, $pattern as xs:string, $flags as xs:string) as xs:string*
+cFunctionCall.functions["tokenize"]	= function(oSequence1, oSequence2, oSequence3) {
+	throw "Function '" + "tokenize" + "' not implemented";
+};
