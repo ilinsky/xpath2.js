2df7820f16f227b9f64ca4766bea8f4b1580bf76
ilinsky <sergey@ilinsky.com>
2012-10-27 17:45:16 +0200

    Implemented rest of functions and pass-through for nilled

--- src/functions/accessor.js
+++ src/functions/accessor.js
@@ -20,20 +20,50 @@
 
 // fn:node-name($arg as node()?) as xs:QName?
 cFunctionCall.functions["node-name"]	= function(oSequence1) {
+	if (arguments.length < 1)
+		throw new cXPath2Error("XPST0017");
+
 	var oSequence	= new cXPath2Sequence;
 	if (!oSequence1.items.length)
 		return oSequence;
 	//
 	var oNode	= oSequence1.items[0];
-	if (cXPath2.DOMAdapter.isNode(oNode))
-		oSequence.add(cXPath2.DOMAdapter.getProperty(oNode, "nodeName"));
+	if (cXPath2.DOMAdapter.isNode(oNode)) {
+		switch (cXPath2.DOMAdapter.getProperty(oNode, "nodeType")) {
+			case 1:	// ELEMENT_NAME
+			case 2:	// ATTRIBUTE_NODE
+				oSequence.add(new cQName(cXPath2.DOMAdapter.getProperty(oNode, "prefix"), cXPath2.DOMAdapter.getProperty(oNode, "localName"), cXPath2.DOMAdapter.getProperty(oNode, "namespaceURI")));
+				break;
+			case 5:	// ENTITY_REFERENCE_NODE
+				throw "Not implemented";
+			case 6:	// ENTITY_NODE
+				throw "Not implemented";
+			case 7:	// PROCESSING_INSTRUCTION_NODE
+				oSequence.add(new cQName(null, cXPath2.DOMAdapter.getProperty(oNode, "target"), null));
+				break;
+			case 10:// DOCUMENT_TYPE_NODE
+				oSequence.add(new cQName(null, cXPath2.DOMAdapter.getProperty(oNode, "name"), null));
+				break;
+		}
+	}
 	//
 	return oSequence;
 };
 
 // fn:nilled($arg as node()?) as xs:boolean?
 cFunctionCall.functions["nilled"]	= function(oSequence1) {
-	throw "Function 'nilled' not implemented";
+	if (arguments.length < 1)
+		throw new cXPath2Error("XPST0017");
+
+	var oSequence	= new cXPath2Sequence;
+	if (!oSequence1.items.length)
+		return oSequence;
+
+	var oNode	= oSequence1.items[0];
+	if (cXPath2.DOMAdapter.isNode(oNode) && cXPath2.DOMAdapter.getProperty(oNode, "nodeType") == 1)
+		oSequence.add(false);	// TODO: Detrmine if node is nilled
+
+	return oSequence;
 };
 
 // fn:string() as xs:string
@@ -46,7 +76,10 @@ cFunctionCall.functions["string"]	= function(/*[*/oSequence1/*]*/) {
 
 // fn:data($arg as item()*) as xs:anyAtomicType*
 cFunctionCall.functions["data"]		= function(oSequence1) {
-	throw "Function 'data' not implemented";
+	if (arguments.length < 1)
+		throw new cXPath2Error("XPST0017");
+
+	return cXPath2Sequence.atomize(oSequence1);
 };
 
 // fn:base-uri() as xs:anyURI?
@@ -70,6 +103,9 @@ cFunctionCall.functions["base-uri"]		= function(oSequence1) {
 
 // fn:document-uri($arg as node()?) as xs:anyURI?
 cFunctionCall.functions["document-uri"]		= function(oSequence1) {
+	if (arguments.length < 1)
+		throw new cXPath2Error("XPST0017");
+
 	var oSequence	= new cXPath2Sequence;
 	//
 	var oNode	= oSequence1.items[0];
