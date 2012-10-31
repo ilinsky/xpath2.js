106277802b94b9772573a538caf9383c981983b4
ilinsky <sergey@ilinsky.com>
2012-10-31 17:06:52 +0200

    Implemented operators on xs:dayTimeDuration

--- src/types/XSDayTimeDuration.js
+++ src/types/XSDayTimeDuration.js
@@ -37,6 +37,15 @@ function fXSDayTimeDuration_toSeconds(oDuration) {
 	return (((oDuration.day * 24 + oDuration.hour) * 60 + oDuration.minute) * 60 + oDuration.second) * (oDuration.negative ? -1 : 1);
 };
 
+function fXSDayTimeDuration_fromSeconds(nValue) {
+	var bNegative	=(nValue = cMath.round(nValue)) < 0,
+		nDays	= ~~((nValue = cMath.abs(nValue)) / 86400),
+		nHours	= ~~((nValue -= nDays * 3600 * 24) / 3600),
+		nMinutes= ~~((nValue -= nHours * 3600) / 60),
+		nSeconds	= nValue -= nMinutes * 60;
+	return new cXSDayTimeDuration(nDays, nHours, nMinutes, nSeconds, bNegative);
+};
+
 function fXSDayTimeDuration_normalize(oDuration) {
 	if (oDuration.second >= 60) {
 		oDuration.minute	+= ~~(oDuration.second / 60);
