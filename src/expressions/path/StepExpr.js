b804a0211b671784f7128931860db854701eae52
ilinsky <sergey@ilinsky.com>
2012-11-05 14:49:16 +0200

    Removed unneccessary TODO statement

--- src/expressions/path/StepExpr.js
+++ src/expressions/path/StepExpr.js
@@ -41,15 +41,16 @@ cStepExpr.prototype.applyPredicates	= function(oContext, oSequence) {
 	var oSequence1,
 		oContext1,
 		oSequence2;
-	// TODO: Apply predicates
+
 	for (var nPredicateIndex = 0, nPredicateLength = this.predicates.length; nPredicateIndex < nPredicateLength; nPredicateIndex++) {
 		oSequence1	= oSequence;
 		oContext1	= new cXPath2Context;
 		oSequence	= new cXPath2Sequence;
 		for (var nIndex = 0, nLength = oSequence1.items.length; nIndex < nLength; nIndex++) {
-			oContext1.last		= oSequence1.items.length;
+			oContext1.last		= nLength;
 			oContext1.context	= oSequence1.items[nIndex];
 			oContext1.position	= nIndex + 1;
+			//
 			oSequence2	= this.predicates[nPredicateIndex].evaluate(oContext1);
 			//
 			if (oSequence2.isSingleton() && typeof oSequence2.items[0] == "number") {
