58261c32f6e10393a5ae7080b6adf8df045c8eee
ilinsky <sergey@ilinsky.com>
2012-11-12 17:07:00 +0200

    Added xs:anyURI type, not fully integrated though

--- src/functions/anyuri.js
+++ src/functions/anyuri.js
@@ -30,39 +30,27 @@ fFunctionCall_defineSystemFunction("resolve-uri",	[[cXSString, '?'], [cXSString,
 
 	if (oSequence1.isEmpty())
 		return null;
+
 	//
-	return fFunctionCall_anyuri_resolveUri(oSequence1.items[0], sBaseUri);
-});
-
-var hFunctionCall_anyuri_cache	= {};
-/*
- * Returns an array of uri components:
- * [scheme, authority, path, query, fragment]
- */
-function fFunctionCall_anyuri_getUriComponents(sUri) {
-	var aResult	= hFunctionCall_anyuri_cache[sUri] ||(hFunctionCall_anyuri_cache[sUri] = sUri.match(/^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/));
-	return [aResult[1], aResult[3], aResult[5], aResult[6], aResult[8]];
-};
-
-function fFunctionCall_anyuri_resolveUri(sUri, sBaseUri) {
+	var sUri	= oSequence1.items[0];
 	if (sUri == '' || sUri.charAt(0) == '#')
 		return sBaseUri;
 
-	var aUri	= fFunctionCall_anyuri_getUriComponents(sUri);
-	if (aUri[0])	// scheme
+	var oUri	= cXSAnyURI.cast(sUri);
+	if (oUri.scheme)
 		return sUri;
 
-	var aBaseUri	= fFunctionCall_anyuri_getUriComponents(sBaseUri);
-	aUri[0]	= aBaseUri[0];	// scheme
+	var oBaseUri	= cXSAnyURI.cast(sBaseUri);
+	oUri.scheme	= oBaseUri.scheme;
 
-	if (!aUri[1]) {
+	if (!oUri.authority) {
 		// authority
-		aUri[1]	= aBaseUri[1];
+		oUri.authority	= oBaseUri.authority;
 
 		// path
-		if (aUri[2].charAt(0) != '/') {
-			var aUriSegments		= aUri[2].split('/'),
-				aBaseUriSegments	= aBaseUri[2].split('/');
+		if (oUri.path.charAt(0) != '/') {
+			var aUriSegments		= oUri.path.split('/'),
+				aBaseUriSegments	= oBaseUri.path.split('/');
 			aBaseUriSegments.pop();
 
 			var nBaseUriStart	= aBaseUriSegments[0] == '' ? 1 : 0;
@@ -81,21 +69,10 @@ function fFunctionCall_anyuri_resolveUri(sUri, sBaseUri) {
 			}
 			if (aUriSegments[--nIndex] == '..' || aUriSegments[nIndex] == '.')
 				aBaseUriSegments.push('');
-			aUri[2]	= aBaseUriSegments.join('/');
+			//
+			oUri.path	= aBaseUriSegments.join('/');
 		}
 	}
 
-	var aResult	= [];
-	if (aUri[0])
-		aResult.push(aUri[0]);
-	if (aUri[1])	// '//'
-		aResult.push(aUri[1]);
-	if (aUri[2])
-		aResult.push(aUri[2]);
-	if (aUri[3])	// '?'
-		aResult.push(aUri[3]);
-	if (aUri[4])	// '#'
-		aResult.push(aUri[4]);
-
-	return aResult.join('');
-};
+	return oUri.toString();
+});
