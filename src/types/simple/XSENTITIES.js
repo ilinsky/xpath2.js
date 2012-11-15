38da7483a42ce8def99629ff9181fca2cc27cec5
ilinsky <sergey@ilinsky.com>
2012-11-16 00:29:25 +0200

    Added copyright note

--- src/types/simple/XSENTITIES.js
+++ src/types/simple/XSENTITIES.js
@@ -1,3 +1,12 @@
+/*
+ * XPath2.js - Pure JavaScript implementation of XPath 2.0 parser and evaluator
+ *
+ * Copyright (c) 2012 Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
+ *
+ *
+ */
+
 function cXSENTITIES() {
 
 };
