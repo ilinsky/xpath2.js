12c1f834de16daeab06e71b33bd1d5504a47279c
ilinsky <sergey@ilinsky.com>
2012-11-15 21:52:26 +0200

    Added implementation of compareDocumentPosition for IE adapter

--- dom/adapters/MSXMLDOMAdapter.js
+++ dom/adapters/MSXMLDOMAdapter.js
@@ -27,9 +27,35 @@ oMSXMLDOMAdapter.isSameNode	= function(oNode, oNode2) {
 	return oNode == oNode2;
 };
 
-oMSXMLDOMAdapter.compareDocumentPosition	= function(oNode, oNode2) {
-	// TODO
-	return true;//oNode.compareDocumentPosition(oNode2);
+oMSXMLDOMAdapter.compareDocumentPosition	= function(oNode, oChild) {
+	if (oChild == oNode)
+		return 0;
+
+	var aChain1	= [], nLength1, oNode1,
+		aChain2	= [], nLength2, oNode2,
+		oElement, nIndex;
+	//
+	for (oElement = oNode; oElement; oElement = oElement.parentNode)
+		aChain1.push(oElement);
+	for (oElement = oChild; oElement; oElement = oElement.parentNode)
+		aChain2.push(oElement);
+	// If nodes are from different documents or if they do not have common top, they are disconnected
+	if (((oNode.ownerDocument || oNode) != (oChild.ownerDocument || oChild)) || (aChain1[aChain1.length - 1] != aChain2[aChain2.length - 1]))
+		return 32 /* cNode.DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC */ | 1 /* cNode.DOCUMENT_POSITION_DISCONNECTED */;
+	//
+	for (nIndex = Math.min(nLength1 = aChain1.length, nLength2 = aChain2.length); nIndex; --nIndex)
+		if ((oNode1 = aChain1[--nLength1]) != (oNode2 = aChain2[--nLength2])) {
+			if (!oNode2.nextSibling)
+				return 4 /* cNode.DOCUMENT_POSITION_FOLLOWING */;
+			if (!oNode1.nextSibling)
+				return 2 /* cNode.DOCUMENT_POSITION_PRECEDING */;
+			for (oElement = oNode2.previousSibling; oElement; oElement = oElement.previousSibling)
+				if (oElement == oNode1)
+					return 4 /* cNode.DOCUMENT_POSITION_FOLLOWING */;
+			return 2 /* cNode.DOCUMENT_POSITION_PRECEDING */;
+		}
+	//
+	return nLength1 < nLength2 ? 4 /* cNode.DOCUMENT_POSITION_FOLLOWING */ | 16 /* cNode.DOCUMENT_POSITION_CONTAINED_BY */ : 2 /* cNode.DOCUMENT_POSITION_PRECEDING */ | 8 /* cNode.DOCUMENT_POSITION_CONTAINS */;
 };
 
 oMSXMLDOMAdapter.lookupPrefix	= function(oNode, sNameSpaceURI) {
