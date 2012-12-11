133c19320c49cacd75ee67355b897feb4b9dd2c6
ilinsky <sergey@ilinsky.com>
2012-12-11 11:44:45 +0200

    Throw exception if context item is not available

--- src/expressions/primary/ContextItemExpr.js
+++ src/expressions/primary/ContextItemExpr.js
@@ -21,5 +21,12 @@ function fContextItemExpr_parse (oLexer, oStaticContext) {
 
 // Public members
 cContextItemExpr.prototype.evaluate	= function (oContext) {
-	return oContext.item == null ? [] : [oContext.item];
+	if (oContext.item == null)
+		throw new cException("XPDY0002"
+//->Debug
+				, "Dynamic context does not have context item initialized"
+//<-Debug
+		);
+	//
+	return [oContext.item];
 };
