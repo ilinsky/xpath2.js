491b7fab995b4af2339ed98fe72d01050199bd8f
Sergey Ilinsky <sergey@ilinsky.com>
2021-03-10 09:38:26 +0100

    Aligned code styling

--- test/expressions/arithmetic/test.js
+++ test/expressions/arithmetic/test.js
@@ -2,57 +2,96 @@ var xpath = require('./../../../api/xpath');
 var expect = require('chai').expect;
 
 describe("arithmetics", function() {
-    it('testAdditiveAdd', function() {
-        expect(xpath.evaluate("3 + 2")[0]).to.equal(5);
+    describe("+", function() {
+        it('', function() {
+            expect(xpath.evaluate("3 + 2")[0])
+                .to.equal(5);
+        });
     });
-    it('testAdditiveSub', function() {
-        expect(xpath.evaluate("2 - 3")[0]).to.equal(-1);
+
+    describe("-", function() {
+        it('', function() {
+            expect(xpath.evaluate("2 - 3")[0])
+                .to.equal(-1);
+        });
     });
-    it('testAdditiveAddSub', function() {
-        expect(xpath.evaluate("3 + 2 - 4")[0]).to.equal(1);
+
+    describe("unary", function() {
+        it('', function() {
+            expect(xpath.evaluate("-2")[0])
+                .to.equal(-2);
+        });
+        it('', function() {
+            expect(xpath.evaluate("+2")[0])
+                .to.equal(2);
+        });
     });
-    it('testUnaryMinus', function() {
-        expect(xpath.evaluate("-2")[0]).to.equal(-2);
+
+    describe("*", function() {
+        it('', function() {
+            expect(xpath.evaluate("2 * 3")[0])
+                .to.equal(6);
+        });
+        it('', function() {
+            expect(xpath.evaluate("2 * 3 * 4")[0])
+                .to.equal(24);
+        });
+        it('', function() {
+            expect(xpath.evaluate("2 *-3")[0])
+                .to.equal(-6);
+        });
     });
-    it('testUnaryPlus', function() {
-        expect(xpath.evaluate("+2")[0]).to.equal(2);
+
+    describe("div", function() {
+        it('', function() {
+            expect(xpath.evaluate("6 div 3")[0])
+                .to.equal(2);
+        });
+        it('', function() {
+            expect(xpath.evaluate("6 div 3 div 2")[0])
+                .to.equal(1);
+        });
+        it('', function() {
+            expect(xpath.evaluate("1 div 2")[0])
+                .to.equal(0.5);
+        });
     });
-    it('testMultiplicativeMul', function() {
-        expect(xpath.evaluate("2 * 3")[0]).to.equal(6);
+
+    describe("idiv", function() {
+        it('', function() {
+            expect(xpath.evaluate("5 idiv 3")[0])
+                .to.equal(1);
+        });
     });
-    it('testMultiplicativeMulMul', function() {
-        expect(xpath.evaluate("2 * 3 * 4")[0]).to.equal(24);
+
+    describe("mod", function() {
+        it('', function() {
+            expect(xpath.evaluate("5 mod 3")[0])
+                .to.equal(2);
+        });
     });
-    it('testMultiplicativeMulNegative', function() {
-        expect(xpath.evaluate("2 *-3")[0]).to.equal(-6);
-    });
-    it('testMultiplicativeDiv', function() {
-        expect(xpath.evaluate("6 div 3")[0]).to.equal(2);
-    });
-    it('testMultiplicativeDivDiv', function() {
-        expect(xpath.evaluate("6 div 3 div 2")[0]).to.equal(1);
-    });
-    it('testMultiplicativeDivDouble', function() {
-        expect(xpath.evaluate("1 div 2")[0]).to.equal(0.5);
-    });
-    it('testMultiplicativeIdiv', function() {
-        expect(xpath.evaluate("5 idiv 3")[0]).to.equal(1);
-    });
-    it('testMultiplicativeMod', function() {
-        expect(xpath.evaluate("5 mod 3")[0]).to.equal(2);
-    });
+
     // Test evaluation order
-    it('testOrderMulAdd', function() {
-        expect(xpath.evaluate("3 * 4 + 2")[0]).to.equal(14);
+    it('', function() {
+        expect(xpath.evaluate("3 + 2 - 4")[0])
+            .to.equal(1);
     });
-    it('testOrderMulAddInBrackets', function() {
-        expect(xpath.evaluate("3 * (4 + 2)")[0]).to.equal(18);
+
+    it('', function() {
+        expect(xpath.evaluate("3 * 4 + 2")[0])
+            .to.equal(14);
     });
-    it('testOrderAddMul', function() {
-        expect(xpath.evaluate("3 + 2 * 4")[0]).to.equal(11);
+    it('', function() {
+        expect(xpath.evaluate("3 * (4 + 2)")[0])
+            .to.equal(18);
+    });
+    it('', function() {
+        expect(xpath.evaluate("3 + 2 * 4")[0])
+            .to.equal(11);
     });
     // Misc
-    it('testAdditiveDouble', function() {
-        expect(xpath.evaluate("0.1 + 0.2")[0]).to.equal(0.3);
+    it('', function() {
+        expect(xpath.evaluate("0.1 + 0.2")[0])
+            .to.equal(0.3);
     });
 });
--- test/expressions/quantified/test.js
+++ test/expressions/quantified/test.js
@@ -5,59 +5,55 @@ describe("quantified", function() {
     describe("every satisfies", function() {
         // Not W3C tests
         it('', function() {
-            expect(xpath.evaluate('every $a in (1, 2, 3, 4, 5) satisfies $a < 3')).to.have.ordered.members([false]);
+            expect(xpath.evaluate('every $a in (1, 2, 3, 4, 5) satisfies $a < 3'))
+                .to.have.ordered.members([false]);
         });
         it('', function() {
-            expect(xpath.evaluate('every $a in (1, 2, 3, 4, 5) satisfies $a < 6')).to.have.ordered.members([true]);
+            expect(xpath.evaluate('every $a in (1, 2, 3, 4, 5) satisfies $a < 6'))
+                .to.have.ordered.members([true]);
         });
         it('', function() {
-            expect(xpath.evaluate('every $a in (1, 2, 3, 4, 5) satisfies $a > 6')).to.have.ordered.members([false]);
-        });
-    });
-
-    describe("every satisfies (empty sequence)", function() {
-        // Not W3C tests
-        it('', function() {
-            expect(xpath.evaluate('every $a in () satisfies $a < 3')).to.have.ordered.members([true]);
-        });
-    });
-
-    describe("every satisfies (item)", function() {
-        // Not W3C tests
-        it('', function() {
-            expect(xpath.evaluate('every $a in 1 satisfies $a < 3')).to.have.ordered.members([true]);
+            expect(xpath.evaluate('every $a in (1, 2, 3, 4, 5) satisfies $a > 6'))
+                .to.have.ordered.members([false]);
         });
         it('', function() {
-            expect(xpath.evaluate('every $a in 5 satisfies $a < 3')).to.have.ordered.members([false]);
+            expect(xpath.evaluate('every $a in () satisfies $a < 3'))
+                .to.have.ordered.members([true]);
+        });
+        it('', function() {
+            expect(xpath.evaluate('every $a in 1 satisfies $a < 3'))
+                .to.have.ordered.members([true]);
+        });
+        it('', function() {
+            expect(xpath.evaluate('every $a in 5 satisfies $a < 3'))
+                .to.have.ordered.members([false]);
         });
     });
 
     describe("some satisfies", function() {
         // Not W3C tests
         it('', function() {
-            expect(xpath.evaluate('some $a in (1, 2, 3, 4, 5) satisfies $a < 3')).to.have.ordered.members([true]);
+            expect(xpath.evaluate('some $a in (1, 2, 3, 4, 5) satisfies $a < 3'))
+                .to.have.ordered.members([true]);
         });
         it('', function() {
-            expect(xpath.evaluate('some $a in (1, 2, 3, 4, 5) satisfies $a < 6')).to.have.ordered.members([true]);
+            expect(xpath.evaluate('some $a in (1, 2, 3, 4, 5) satisfies $a < 6'))
+                .to.have.ordered.members([true]);
         });
         it('', function() {
-            expect(xpath.evaluate('some $a in (1, 2, 3, 4, 5) satisfies $a > 6')).to.have.ordered.members([false]);
-        });
-    });
-
-    describe("some satisfies (empty sequence)", function() {
-        // Not W3C tests
-        it('', function() {
-            expect(xpath.evaluate('some $a in () satisfies $a < 3')).to.have.ordered.members([false]);
-        });
-    });
-
-    describe("some satisfies (item)", function() {
-        // Not W3C tests
-        it('', function() {
-            expect(xpath.evaluate('some $a in 1 satisfies $a < 3')).to.have.ordered.members([true]);
+            expect(xpath.evaluate('some $a in (1, 2, 3, 4, 5) satisfies $a > 6'))
+                .to.have.ordered.members([false]);
         });
         it('', function() {
-            expect(xpath.evaluate('some $a in 5 satisfies $a < 3')).to.have.ordered.members([false]);
+            expect(xpath.evaluate('some $a in () satisfies $a < 3'))
+                .to.have.ordered.members([false]);
+        });
+        it('', function() {
+            expect(xpath.evaluate('some $a in 1 satisfies $a < 3'))
+                .to.have.ordered.members([true]);
+        });
+        it('', function() {
+            expect(xpath.evaluate('some $a in 5 satisfies $a < 3'))
+                .to.have.ordered.members([false]);
         });
     });});
